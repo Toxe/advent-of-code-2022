1b3af8135d861b7ea24e269fcf31247c0116e544
Toxe <toxe@toxe.net>
2022-12-03 11:51:22 +0100

    typo

--- src/day02/day02.hpp
+++ src/day02/day02.hpp
@@ -3,5 +3,5 @@
 #include <string>
 #include <vector>
 
-int day02_part1(const std::vector<std::string>& current_sum);
+int day02_part1(const std::vector<std::string>& lines);
 int day02_part2(const std::vector<std::string>& lines);
