22f15fefebb7fe7ad6da6f83776e92955876a005
Toxe <toxe@toxe.net>
2022-12-15 15:02:17 +0100

    Rename shared library to libaoc

--- src/day01/day01.hpp
+++ src/day01/day01.hpp
@@ -2,7 +2,7 @@
 
 #include <vector>
 
-#include "../shared/read_grouped_numbers.hpp"
+#include "../libaoc/read_grouped_numbers.hpp"
 
 int day01_part1(const GroupedNumbers& grouped_numbers);
 int day01_part2(const GroupedNumbers& grouped_numbers);
